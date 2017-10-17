ecf2f43ce0b9f09aa5d1cc166585c5fd50d72ce0
iceklue <iceklue@gmail.com>
2017-10-17 16:37:57 +0200

    ok

--- The Apocalypse/Assets/PeterAI.cs
+++ The Apocalypse/Assets/PeterAI.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PeterAI : MonoBehaviour {
-
+    public float MoveFactor = 1;
+    public Transform player;
+    private NavMeshAgent navAgent;
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        navAgent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        MoveAway();
 	}
+    public void MoveAway()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - player.position);
+            
+        Vector3 runTo = transform.position + transform.forward * MoveFactor;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetAreaFromName("Walkable"));
+
+        navAgent.SetDestination(hit.position);
+    }
 }
---
+++ The Apocalypse/Assets/TileManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+    public int renderDistance=5;
+    public GameObject[] tiles;
+    public int tileSize;    
+    [SerializeField]
+    private Transform player;
+    private Dictionary<Vector2, GameObject> tileSet = new Dictionary<Vector2, GameObject>();
+  
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine("CheckTiles");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator CheckTiles()
+    {
+        while (true)
+        {
+            int closeX = (((int)player.position.x + (tileSize / 2)) / tileSize) * tileSize;
+            int closeY = (((int)player.position.z + (tileSize / 2)) / tileSize) * tileSize;
+            Vector2 pos;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+        
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private IEnumerator SpawnTile(Vector2 position)
+    {
+        yield break;
+    }
+    private IEnumerator RemoveTile(Vector2 position)
+    {
+        yield break;
+    }
+}
